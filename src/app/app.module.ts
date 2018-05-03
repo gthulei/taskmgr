1365fc5ca67e87dcae863164de9721bed19524ec
hulei <hulei@nonobank.com>
2018-05-03 10:46:13 +0800

    路由配置

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginModule } from './login/login.module';
 
 
 @NgModule({
@@ -12,7 +14,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    AppRoutingModule,
+    LoginModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
